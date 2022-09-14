fe1eac45e880283c61eac786c227cbf122dbc88c
Sayeednayeem1 <sayeedn850@gmail.com>
2022-09-15 00:09:32 +0600

    countdown section display none added

--- history.js
+++ history.js
@@ -18,7 +18,7 @@ function addHistory(questionText, timeTaken, errorCount) {
   previousTests.push({ questionText, timeTaken, errorCount });
   localStorage.setItem("testHistory", JSON.stringify(previousTests));
 
-  // displayHistory();
+  displayHistory();
 }
 
 function displayHistory() {
